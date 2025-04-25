668a96b41b61ec1526716b6df7275a602a825f75
Bailey Thorp <bailey.thorp@techtorium.ac.nz>
2025-04-25 14:21:31 +1200

    made a simple frontend API call on the index page. Remember to run backend/server.js so the backend works.

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,32 @@
 import { Link } from "expo-router";
 import { Text, View} from "react-native";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'react-native-gesture-handler' //makes drawers work
 
 const Index = () => {
+
+  //192.168.1.220 is my local IP. you will likely need to change this with your own IPv4 address (find with command 'ipconfig'). We can make a gitignore or automate this later
+  const apiUrl = "http://192.168.1.220:5000/api/recipes"
+
+  const [recipes, setRecipes] = useState([]);
+
+  //function to do API call to get all recipes
+  //Remember you need to run backend/server.js in a new terminal to make the backend work
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get(apiUrl); //this axios.get() looks at the backend (server.js) and does the GET function with the /api/recipes route
+      setRecipes(response.data)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  //call the function to get recipes
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
   return (
     <View
       style={{
@@ -15,11 +39,18 @@ const Index = () => {
       <Text>
         Try writirng different text here and saving it to see the app update
         live. You may need to press 'r' in the terminal before hot reload starts
-        working.
+        working. {"\n"}
       </Text>
+
+      <Text>Links:</Text>
       <Link href="/search">go to Search screen</Link>
       <Link href="/register">go to Register screen</Link>
       <Link href="/login">go to Login screen</Link>
+
+      <Text>{"\n"}Recipes from the database:</Text>
+      {recipes.map((recipe, index) => (
+        <Text key={index}>{index}. {recipe.title} - {recipe.ingredients}</Text> //idk how to remove these errors, but it works as expected
+      ))}
 
     </View>
   );
