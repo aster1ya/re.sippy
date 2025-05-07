c32a0926bfea3368313b2a5b768434cf34d1adb2
m1juku <kaahotaylor@gmail.com>
2025-05-08 11:17:57 +1200

    Updated book drawer to filter recipes by saved, display called recipes as separate cards

--- app/(drawers)/book.tsx
+++ app/(drawers)/book.tsx
@@ -1,14 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'expo-router'
-import Index from '..'
+import Card from '../card'
+import axios from 'axios'
 
 const book = () => {
-  return (
-    <View>
-      <Text>My Recipes</Text>
-      <Link href="../recipe_details/pancakes">go to Details screen of Pancake</Link>
-      <Link href="../recipe_details/ramen">go to Details screen of Ramen</Link>
+
+  //copied over code to experiment
+  const apiUrl = "http://localhost:5000/api/recipes";
+
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get(apiUrl); //this axios.get() looks at the backend (server.js) and does the GET function with the /api/recipes route
+      setRecipes(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+ //new code to try and display the data as separate cards (also with card.tsx)
+  const cards = () => {
+    return recipes.map((recipe,index) => (<Card key={recipes._id} recipes={recipes}/>));
+  }
+
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:'white',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+  })
+
+  return(
+    <View style={styles.container}>
+      {cards()}
     </View>
   )
 }
