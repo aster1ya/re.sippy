e0fad0ea0f3b75abdea878d5c219dbc5285a655d
baileythorp04 <baileythorp04@gmail.com>
2025-05-16 12:26:00 +1200

    Created 'RecipeList' component. To be reused in recipebook, search, favourites etc. Still could be improved with styling and/or showing more details

--- app/book.tsx
+++ app/book.tsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useRouter } from "expo-router";
 import Card from "./card";
 import axios from "axios";
-import { GetAllRecipes } from "../controller";
+import { GetAllRecipes, SearchRecipes } from "../controller";
 import IRecipe from "../types/Recipe";
+import RecipeList from "../components/RecipeList";
 
 const book = () => {
   const apiUrl = "http://localhost:5000/api/recipes";
@@ -32,21 +33,8 @@ const book = () => {
 
   return (
     <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.title}>All Recipes</Text>
-        {recipes.map((recipe, index) => (
-          <View key={index} style={styles.recipePreview}>
-            <View style={styles.row}>
-              <Text style={styles.header}>{recipe.title}</Text>
-              <Button
-                title="View Recipe"
-                onPress={() => goToRecipe(recipe._id)}
-              />
-            </View>
-            <Text>{recipe.description}</Text>
-          </View>
-        ))}
-      </View>
+      <Text style={styles.title}>All Recipes</Text>
+      <RecipeList recipes={recipes} title="All Recipes" />
     </ScrollView>
   );
 };
@@ -54,31 +42,9 @@ const book = () => {
 export default book;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-  },
-
-  recipePreview: {
-    marginBottom: 30,
-    backgroundColor: "#e0e0e0",
-    padding: 10,
-  },
-
-  header: {
-    fontSize: 26,
-  },
-
   title: {
     fontSize: 30,
     alignSelf: "center",
     margin: 15,
-  },
-
-  row: {
-    flexDirection: "row",
-    display: "flex",
-    justifyContent: "space-between",
   },
 });
---
+++ components/RecipeList.tsx
@@ -0,0 +1,63 @@
+import { Text, StyleSheet, View, Button } from "react-native";
+import React, { Component } from "react";
+import IRecipe from "../types/Recipe";
+import { useRouter } from "expo-router";
+
+type RecipeListProps = {
+  recipes: IRecipe[];
+  title: string;
+};
+
+const RecipeList = ({ recipes, title }: RecipeListProps) => {
+  const router = useRouter();
+
+  const goToRecipe = (recipeId: string) => {
+    router.push({
+      pathname: "/recipe_details/[id]",
+      params: { id: recipeId },
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      {recipes.map((recipe, index) => (
+        <View key={index} style={styles.recipePreview}>
+          <View style={styles.row}>
+            <Text style={styles.header}>{recipe.title}</Text>
+            <Button
+              title="View Recipe"
+              onPress={() => goToRecipe(recipe._id)}
+            />
+          </View>
+          <Text>{recipe.description}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default RecipeList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
+
+  recipePreview: {
+    marginBottom: 30,
+    backgroundColor: "#e0e0e0",
+    padding: 10,
+  },
+
+  header: {
+    fontSize: 26,
+  },
+
+  row: {
+    flexDirection: "row",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+});
